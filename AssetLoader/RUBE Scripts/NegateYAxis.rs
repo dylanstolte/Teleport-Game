df939513636f075ed2cb3e0ce8c6aa573f8d644e
unknown <stolted@STOLTED2.auth.hpicorp.net>
2016-03-11 15:52:51 -0700

    RUBE script work

--- AssetLoader/RUBE Scripts/NegateYAxis.rs
+++ AssetLoader/RUBE Scripts/NegateYAxis.rs
@@ -3,6 +3,10 @@ select(verticies);
 
 for(uint i = 0; i < verticies.length; i ++)
 {
+	fixture tempFix = verticies[i].getFixture();
+	body tempBody = tempFix.getBody();
+	vec2 posBody = tempBody.getWorldCenter();
+
 	verticies[i].setWorldPos(verticies[i].wpos.x,
-						-(verticies[i].wpos.y));
+						-(verticies[i].wpos.y) + posBody.y*2 );
 }
---
+++ untitled1.json-backups/untitled1.json-scriptLog-160311-111324.rs
@@ -0,0 +1,2 @@
+//Started script log at 03/11/16 11:13:24
+
